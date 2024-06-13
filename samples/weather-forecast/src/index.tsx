b8d515e34feb29806d93404313e23c6f46dc856c
Marcel Seidel <marcel@staffbase.com>
2024-06-13 09:05:43 +0200

    chore: Setting correct path for PNG file

--- samples/weather-forecast/src/index.tsx
+++ samples/weather-forecast/src/index.tsx
@@ -23,7 +23,7 @@ import {
 import { WeatherForecastProps, WeatherForecast } from "./weather-forecast";
 import { configurationSchema, uiSchema } from "./configuration-schema";
 import pkg from "../package.json";
-import widgetIcon from "../weather-forecast.png";
+import widgetIcon from "../resources/weather-forecast.png";
 
 /**
  * This factory creates the class which is registered with the tagname in the `custom element registry`
